4ffbc5ae78d404f3c611070c8bb61e1c76ae5a72
Garrett Tongue <garrettjtongue@gmail.com>
2017-10-05 13:16:19 -0700

    Stack and Queue done

---
+++ W1D5/ADT.rb
@@ -0,0 +1,38 @@
+class Stack
+    def initialize
+        @stack = []
+    end
+
+    def add(el)
+        @stack << el
+    end
+
+    def remove
+        @stack.pop
+    end
+
+    def show
+        @stack.dup
+    end
+end
+
+class Queue
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        @queue << el
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def show
+        @queue.dup
+    end
+end
+
+class Map
+end
