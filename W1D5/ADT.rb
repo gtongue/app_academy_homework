8c50d040a8af74d4d05696bef3810afe5cf2756c
Garrett Tongue <garrettjtongue@gmail.com>
2017-10-05 22:12:27 -0700

    Added Map ADT

--- W1D5/ADT.rb
+++ W1D5/ADT.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Stack
     def initialize
         @stack = []
@@ -35,4 +36,52 @@ class Queue
 end
 
 class Map
+    def initialize
+        @map = []
+    end
+
+    def assign(key, val)
+        index = @map.index {|hash| hash[0] == key }
+        if index == nil
+            @map.push([key, val])
+        else
+            @map[index][1] = val
+        end
+    end
+
+    def lookup(key)
+        @map.each {|el| return el[1] if el[0] == key}
+        nil
+    end
+
+    def remove(key)
+        index = @map.index {|hash| hash[0] == key }
+        if index == nil
+            return nil
+        else
+            @map.delete_at(index)
+        end            
+    end
+
+    # def show
+    #     @map.dup
+    # end
+
+    #After looking at solutions this is how show should be done
+    #forgot we needed to use deep_dup because of the pair values
+    def show
+        deep_dup(@map)
+    end
+
+    private
+    def deep_dup(arr)
+        arr.map do |el| 
+            if el.is_a?(Array) 
+                deep_dup(el)
+            else
+                el
+            end
+        end
+    end
+
 end
