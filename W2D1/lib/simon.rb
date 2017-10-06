3edbfa11d4b307e066e4979e055629bfc89d53ec
Garrett Tongue <garrettjtongue@gmail.com>
2017-10-06 13:16:27 -0700

    Simon implemented

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -5,40 +5,64 @@ class Simon
 
   def initialize
     @game_over = false
-    add_random_color
+    @seq = []
+    @sequence_length = 1
   end
 
   def play
-
+    until(@game_over)
+      take_turn 
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    unless @game_over
+      @sequence_length += 1      
+      round_success_message unless @game_over     
+    end
   end
 
-  def show_sequencesdad
-    
+  def show_sequence
+    add_random_color        
+    system('clear')
+    @seq.each do |color|
+      puts color.to_s
+      sleep(1)
+      system('clear')
+    end
   end
 
   def require_sequence
-
+    puts "Please enter each color ('r', 'g', 'b', 'y')in the order they appeared (hit enter after every input)"
+    @sec.each do |color|
+      input = gets.chomp
+      if input[0] != color
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
     color = COLORS.sample
-    seq << color
-    sequence_length += 1
+    @seq << color
   end
 
   def round_success_message
-    
+    "Round successfull!"
   end
 
   def game_over_message
-
+    "You lost!"
   end
 
   def reset_game
-
+    @game_over = false
+    @seq = []
+    @sequence_length = 1
   end
 end
